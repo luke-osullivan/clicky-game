7bb92704d5b49d19a742a8b2bad1411605883197
Luke O'Sullivan <luke@Lukes-MacBook.local>
2019-03-05 12:59:15 -0500

    Updates

---
+++ precache-manifest.fd8401ebedd6c43e08820d6c6102f39e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "b854f57c4615d31688ba",
+    "url": "/clicky-game/static/js/main.b854f57c.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "c4b4c0fe4cfb94e1e5166c5266cb7a16",
+    "url": "/clicky-game/index.html"
+  }
+];
